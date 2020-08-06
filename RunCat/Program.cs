01f9b1192f67841b9b4389d29dd4972196aa2428
Kyome22 <kyomesuke@gmail.com>
2020-08-07 02:23:11 +0900

    first commit

---
+++ RunCat/Program.cs
@@ -0,0 +1,127 @@
+﻿// Copyright 2020 Takuto Nakamura
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+using RunCat.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace RunCat
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RunCatApplicationContext());
+        }
+    }
+
+    public class RunCatApplicationContext: ApplicationContext
+    {
+        private PerformanceCounter cpuUsage;
+        private NotifyIcon notifyIcon;
+        private int current = 0;
+        private Icon[] icons;
+        private Timer animateTimer = new Timer();
+        private Timer cpuTimer = new Timer();
+        
+      
+        public RunCatApplicationContext()
+        {
+            cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            notifyIcon = new NotifyIcon()
+            {
+                Icon = Resources.cat0,
+                ContextMenu = new ContextMenu(new MenuItem[]
+                {
+                    new MenuItem("Exit", Exit)
+                }),
+                Text = "0.0%",
+                Visible = true
+            };
+
+            icons = new List<Icon>
+            {
+                Resources.cat0,
+                Resources.cat1,
+                Resources.cat2,
+                Resources.cat3,
+                Resources.cat4
+            }
+            .ToArray();
+
+            SetAnimation();
+            ObserveCPUTick(null, EventArgs.Empty);
+            StartObserveCPU();
+            current = 1;
+        }
+
+        /*private void NotifyIconMouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("click");
+            if (e.Button == MouseButtons.Left)
+            {
+                Console.WriteLine("gomi");
+                MethodInfo method = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+                method.Invoke(notifyIcon, null);
+            }
+        }*/
+
+        private void Exit(object sender, EventArgs e)
+        {
+            animateTimer.Stop();
+            cpuTimer.Stop();
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void SetAnimation()
+        {
+            animateTimer.Interval = 200;
+            animateTimer.Tick += new EventHandler(AnimationTick);
+        }
+
+        private void AnimationTick(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = icons[current];
+            current = (current + 1) % icons.Length;
+        }
+
+        private void StartObserveCPU()
+        {
+            cpuTimer.Interval = 3000;
+            cpuTimer.Tick += new EventHandler(ObserveCPUTick);
+            cpuTimer.Start();
+        }
+
+        private void ObserveCPUTick(object sender, EventArgs e)
+        {
+            float s = cpuUsage.NextValue();
+            notifyIcon.Text = String.Format("{0:#.#}%", s);
+            s = 200.0f / (float)Math.Max(1.0f, Math.Min(20.0f, s / 5.0f)); ;
+            Console.WriteLine(s);
+            animateTimer.Stop();
+            animateTimer.Interval = (int)s;
+            animateTimer.Start();
+        }
+
+    }
+}
---
+++ RunCat/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("RunCat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RunCat")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("148b00e2-dafd-4aa6-bdba-3a365047dd9b")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
