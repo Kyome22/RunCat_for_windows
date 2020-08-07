e8ce657416c72cf57d26f9b4cd412633614f07e7
murakamiii <jiw46.xe@gmail.com>
2020-08-08 04:41:32 +0900

    fix cpuUsage Formatting

--- RunCat/Program.cs
+++ RunCat/Program.cs
@@ -133,7 +133,7 @@ namespace RunCat
         private void ObserveCPUTick(object sender, EventArgs e)
         {
             float s = cpuUsage.NextValue();
-            notifyIcon.Text = String.Format("{0:#.#}%", s);
+            notifyIcon.Text = $"{s:f1}%";
             s = 200.0f / (float)Math.Max(1.0f, Math.Min(20.0f, s / 5.0f));
             animateTimer.Stop();
             animateTimer.Interval = (int)s;
