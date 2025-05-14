bf5254e73b479ddab12bced642ba5ea4b3908e1a
Henri <henri@falconforce.nl>
2025-05-14 12:27:13 +0200

    Adding support for party parrot

--- RunCat/Program.cs
+++ RunCat/Program.cs
@@ -47,7 +47,7 @@ namespace RunCat
 
     public class RunCatApplicationContext : ApplicationContext
     {
-        private const int CPU_TIMER_DEFAULT_INTERVAL = 3000;
+        private const int CPU_TIMER_DEFAULT_INTERVAL = 1000;
         private const int ANIMATE_TIMER_DEFAULT_INTERVAL = 200;
         private PerformanceCounter cpuUsage;
         private ToolStripMenuItem runnerMenu;
@@ -93,6 +93,10 @@ namespace RunCat
                 new ToolStripMenuItem("Horse", null, SetRunner)
                 {
                     Checked = runner.Equals("horse")
+                },
+                new ToolStripMenuItem("Party Parrot", null, SetRunner)
+                {
+                    Checked = runner.Equals("party parrot")
                 }
             });
 
@@ -211,20 +215,28 @@ namespace RunCat
         {
             string prefix = 0 < manualTheme.Length ? manualTheme : systemTheme;
             ResourceManager rm = Resources.ResourceManager;
+
             // default runner is cat
             int capacity = 5;
-            if (runner.Equals("parrot"))
+            if (runner.Equals("parrot") || runner.Equals("party parrot"))
             {
                 capacity = 10;
-            } 
-            else if (runner.Equals("horse")) 
+            }
+            else if (runner.Equals("horse"))
             {
                 capacity = 14;
             }
             List<Icon> list = new List<Icon>(capacity);
             for (int i = 0; i < capacity; i++)
             {
-                list.Add((Icon)rm.GetObject($"{prefix}_{runner}_{i}"));
+                if (runner.Equals("party parrot"))
+                {
+                    list.Add((Icon)rm.GetObject($"party_parrot_{i}"));
+                }
+                else
+                {
+                    list.Add((Icon)rm.GetObject($"{prefix}_{runner}_{i}"));
+                }
             }
             icons = list.ToArray();
         }
@@ -263,9 +275,9 @@ namespace RunCat
             else if (speed.Equals("cpu 20%"))
                 minCPU = 50f;
             else if (speed.Equals("cpu 30%"))
-                minCPU = 33f;    
+                minCPU = 33f;
             else if (speed.Equals("cpu 40%"))
-                minCPU = 25f;   
+                minCPU = 25f;
         }
 
         private void SetSpeedLimit(object sender, EventArgs e)
@@ -350,7 +362,7 @@ namespace RunCat
         private void CPUTickSpeed()
         {
             if (!speed.Equals("default"))
-            {            
+            {
                 float manualInterval = (float)Math.Max(minCPU, interval);
                 animateTimer.Stop();
                 animateTimer.Interval = (int)manualInterval;
@@ -383,7 +395,7 @@ namespace RunCat
             cpuTimer.Tick += new EventHandler(ObserveCPUTick);
             cpuTimer.Start();
         }
-        
+
         private void HandleDoubleClick(object Sender, EventArgs e)
         {
             var startInfo = new ProcessStartInfo
